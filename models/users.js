84a9c044a3f7b8e6401c058a31bcccd8ea784fca
raonzena <seven1004z@gmail.com>
2019-05-14 16:58:06 +0900

    20190514 bwb-server login init

--- models/users.js
+++ models/users.js
@@ -4,8 +4,19 @@ module.exports = (sequelize, DataTypes) => {
       userPw: DataTypes.STRING,
       nickname: DataTypes.STRING,
       gender: DataTypes.INTEGER
-    }, {});
+    }, 
+    {
+        hooks: {
+            beforeCreate: (data, options) => {
+            var shasum = crypto.createHash('sha1');
+            shasum.update(data.dataValues.password);
+            data.dataValues.password = shasum.digest('hex');
+            }
+        }
+    });
+    
     users.associate = function(models) {
     };
+
     return users;
 };
