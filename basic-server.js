f264dd2d327a6bd82a7a108fa045851dfe43d764
Seulah CHUNG <sseulsseul@gmail.com>
2019-05-14 21:29:26 +0900

    temp commit

--- basic-server.js
+++ basic-server.js
@@ -2,25 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const parser = require('body-parser');
 const routes = require('./routes/routes');
-const db = require('./dbConnect');
 const app = express();
 
 app.use(cors());
 app.use(parser());
 app.use(routes);
 
-db.query('show tables', (err, rows) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(rows);
-    }
-});
-
-
 // app.use('/', routes);
 app.listen(3000, () => {
-    console.log('server listen on 3000');
+    console.log('chatterbox-serer listen on 3000');
 });
 
 module.exports = app;
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const controllers = require('../controllers/login');
+const signup = require('../controllers/signup');
+// const controllers = require('./controllers');
 
-router.post('/login', controllers.loginController);
+router.post('/signup', signup.postUserController);
+// router.get('/classes/messages', controllers.getMessageController);
 // router.post('/classes/messages', controllers.postMessageController);
-
+router.get('/', (req, res) => {
+    res.send('success');
+});
 module.exports = router;
