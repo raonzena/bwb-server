3c366cd5c9b9ff97c21d876bb7b147ae576d22fa
raonzena <seven1004z@gmail.com>
2019-05-15 11:28:49 +0900

    20190515 temp commit

--- controllers/signup.js
+++ controllers/signup.js
@@ -2,15 +2,37 @@ const users = require('../models').users;
 
 const postUserController = function (req, res) {
     const { body } = req;
-    console.log(req);
-    // users.create({
-    //     userId: body.id,
-    //     userPw: body.pw,
-    //     nickname: body.nick_name,
-    //     gender: body.gender
-    // });
-    res.status(201).send('success');
+    users
+    .create({
+        userId: body.id,
+        userPw: body.pw,
+        nickname: body.nick_name,
+        gender: body.gender
+    })
+    .then(result => {
+        res.status(201).send('success');
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).send('error');
+    });
 };
+
+const idDuplicationCheckController = function (req, res) {
+    const { body } = req;
+    users
+    .count({
+        where: [{userId: body.id}]
+    })
+    .then(result => {
+        res.status(200).send(result);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(404).send('error');
+    });
+};
+
 
 module.exports = {
     postUserController
