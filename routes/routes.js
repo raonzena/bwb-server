c851af361bc86b957ca5960a597d50fb1ac6fdc7
Seulah CHUNG <sseulsseul@gmail.com>
2019-05-15 21:07:42 +0900

    controller/logout templete created

--- routes/routes.js
+++ routes/routes.js
@@ -7,6 +7,7 @@ const meetingDelete = require('../controllers/meetingDelete');
 const newMeeting = require('../controllers/newMeeting');
 const newMember = require('../controllers/newMember');
 const meetingLists = require('../controllers/meetingLists');
+const logout = require('../controllers/logout');
 
 router.post('/signup', signup.postUserController);
 router.post('/login', login.loginController);
@@ -16,7 +17,8 @@ router.post('/meetings/delete/meeting', meetingDelete.postMeetingDeleteControlle
 router.post('/meetings/new/meeting', newMeeting.newMeetingController);
 router.post('/meetings/new/member', newMember.newMemberController);
 router.get('/nickname/check', signup.nicknameDuplicationCheckController);
-router.get('//meetings/list/region', meetingLists.meetingListsController);
+router.get('/meetings/list/region', meetingLists.meetingListsController);
+router.get('/logout', logout.logoutController);
 
 
 router.get('/', (req, res) => {
