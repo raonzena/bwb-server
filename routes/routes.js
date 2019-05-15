b0bca4a295bc2fd089d9b98df51fe0a6acff8311
Seulah CHUNG <sseulsseul@gmail.com>
2019-05-15 16:04:22 +0900

    feature/newMember initial complete and feature/newMeeting update

--- routes/routes.js
+++ routes/routes.js
@@ -3,15 +3,18 @@ const router = express.Router();
 const signup = require('../controllers/signup');
 const login = require('../controllers/login');
 const newMeeting = require('../controllers/newMeeting');
+const newMember = require('../controllers/newMember');
 
 router.post('/signup', signup.postUserController);
 router.post('/login', login.loginController);
 router.get('/idcheck', signup.idDuplicationCheckController);
 
 router.post('/meetings/new/meeting', newMeeting.newMeetingController);
+router.post('/meetings/new/member', newMember.newMemberController);
 
 
 router.get('/', (req, res) => {
    res.send('success');
 });
+
 module.exports = router;
