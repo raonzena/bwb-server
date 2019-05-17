91ad66dafbc108cf2f0dd51e5042f2356736ee27
seulah chung <sseulsseul@gmail.com>
2019-05-17 11:16:57 +0900

    controller/meetingDetail updated

--- controllers/meetingDetail.js
+++ controllers/meetingDetail.js
@@ -1,30 +1,47 @@
 const meetings = require('../models').meetings;
 const users = require('../models').users;
 const members = require('../models').members;
+const verifyOptions = require('../modules/tokenUtil').verifyOptions;
+const jwt = require('jsonwebtoken');
 
 const getMeetingDetailController = function (req, res) {
+    let userId;
+    let token = req.headers.authorization;
+    if (!token) {
+        userId = null;
+    } else {
+        let legit = jwt.verify(token, 'bwb12', verifyOptions)
+        userId = legit.data;
+    }
     const { query } = req;
-    
+
     members
-    .findAll({
-        include: [{
-            model: meetings,
-            where: { id: query.meetingId },
-            include: { model: users }
-        },
-        {
-            model: users
-        }],
-    })
-    .then(results => {
-        res.status(200).json(results);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).send(err);
-    });
+        .findAll({
+            include: [{
+                model: meetings,
+                where: { id: query.meetingId },
+                include: { model: users }
+            },
+            {
+                model: users
+            }],
+        })
+        .then(results => {
+            results = JSON.stringify(results);
+            results = JSON.parse(results);
+            results.push({ userId: userId })
+            return results
+        })
+        .then(results => {
+            res.status(200).json(results);
+
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send(err);
+        });
 };
 
 module.exports = {
-    getMeetingDetailController 
+    getMeetingDetailController
 };
